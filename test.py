3852225d5f722364bd386a1a50dfcc3c93643ee1
罗先祺 <117899112+ImaiChika@users.noreply.github.com>
2026-01-16 17:46:16 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from telethon import TelegramClient
 import socks
-api_id = '32805884'
-api_hash = '0573c570f9296be684a0ebd0c80dedcf'
+api_id = ''
+api_hash = ''
 proxy = (socks.SOCKS5, '127.0.0.1', 7890, True)
 with TelegramClient('anon', api_id, api_hash,proxy=proxy) as client:
    client.loop.run_until_complete(client.send_message('me', 'hello'))
